07ae66c7888b68080f45ddd87fa246bb12e4f7e7
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2022-11-08 13:38:11 +0200

    s

--- js/functions.js
+++ js/functions.js
@@ -13,18 +13,35 @@ window.onload = () => {
 
   //view incidint
   let request = new XMLHttpRequest();
-  request.open("GET", "https://hossam9866.github.io/NWC/api/incident.jsons");
-  //request.setRequestHeader("Authorization", "Auth Example");
 
+  request.open("GET", "https://hossam9866.github.io/NWC/api/incident.json");
+  //request.setRequestHeader("Authorization", "Auth Example");
   request.send();
   request.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       let data = JSON.parse(this.responseText);
-      console.log(data);
-    } else if (this.readyState === 4 && this.status === 404) {
-      console.log("404");
-    } else if (this.readyState === 4 && this.status === 403) {
-      console.log("Forbidden");
+      document.getElementById("incident-name").innerText = data["title"];
+      document.getElementById("for").innerText = data["for"];
+      document.getElementById("state").innerText = data["state"];
+      document.getElementById("level").innerText = data["level"];
+      document.getElementById("asset-type").innerText = data["asset_type"];
+      document.getElementById("service-type").innerText = data["service_type"];
+      document.getElementById("control-room").innerText = data["control_room"];
+      document.getElementById("summary").innerText = data["summary"];
+      document.getElementById("expected-end").innerText = data["expected_end"];
+      document.getElementById("last-updated").innerText = data["last_updated"];
+      document.getElementById("status").innerText = data["status"];
+    }
+  };
+  request.onload = function () {
+    if (this.status === 404) {
+      document.querySelector(
+        ".error-message"
+      ).innerHTML = `404 Incident not found`;
+    } else if (this.status === 403) {
+      document.querySelector(
+        ".error-message"
+      ).innerHTML = `403 Not alloweed to view this incident`;
     }
   };
 };
