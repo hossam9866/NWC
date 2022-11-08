3b9d05e40e124dc6f95af788416cd9182a5be08c
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2022-11-08 11:43:07 +0200

    d

--- js/functions.js
+++ js/functions.js
@@ -12,4 +12,19 @@ window.onload = () => {
   } catch (e) {}
 
   //view incidint
+  let request = new XMLHttpRequest();
+  request.open("GET", "https://hossam9866.github.io/NWC/api/incident.jsons");
+  //request.setRequestHeader("Authorization", "Auth Example");
+
+  request.send();
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      let data = JSON.parse(this.responseText);
+      console.log(data);
+    } else if (this.readyState === 4 && this.status === 404) {
+      console.log("404");
+    } else if (this.readyState === 4 && this.status === 403) {
+      console.log("Forbidden");
+    }
+  };
 };
