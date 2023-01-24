6d75784016393b01e6f056afb6af3aedc6b27bc3
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2023-01-24 09:06:01 +0200

    g

---
+++ php_email/GetData.php
@@ -0,0 +1,5 @@
+<?php
+echo "d";
+    $api = new API("c06e43cd8b5a9acbc0fbf95cc605d9cf8e0cd823","98e3f0d48ec91eca84d67b03a79cf783e32a8168");
+
+?>
---
+++ php_email/index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php include "SimplyCastAPI.php" ?>
+        <?php include "GetData.php" ?>
+    </body>
+</html>
