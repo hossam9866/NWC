87e01731a90eb6e6134c89c73c8ddcc4d6359401
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2023-11-08 09:25:44 +0200

    y

---
+++ code/app.js
@@ -0,0 +1,18 @@
+function alignLinesWithHeaders() {
+  let lineElements = document.querySelectorAll(".line");
+
+  lineElements.forEach((lineElement) => {
+    let sectionElement = lineElement.closest(".section-flex");
+    let titleElement = sectionElement.querySelector(".title-bold");
+    const titlePosTop = titleElement.getBoundingClientRect().top;
+    const sectionPosTop = sectionElement.getBoundingClientRect().top;
+    let linePosTop = Math.abs(titlePosTop - sectionPosTop);
+    lineElement.style.top = `${linePosTop + 22}px`;
+  });
+}
+
+alignLinesWithHeaders();
+
+window.onresize = () => {
+  alignLinesWithHeaders();
+};
