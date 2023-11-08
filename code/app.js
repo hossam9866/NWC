6ae44b867bb3cfb0879b10c39539ab3185677df9
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2023-11-08 11:12:01 +0200

    dd

--- code/app.js
+++ code/app.js
@@ -1,18 +1,20 @@
-function alignLinesWithHeaders() {
-  let lineElements = document.querySelectorAll(".line");
+window.onload = () => {
+  function alignLinesWithHeaders() {
+    let lineElements = document.querySelectorAll(".line");
 
-  lineElements.forEach((lineElement) => {
-    let sectionElement = lineElement.closest(".section-flex");
-    let titleElement = sectionElement.querySelector(".title-bold");
-    const titlePosTop = titleElement.getBoundingClientRect().top;
-    const sectionPosTop = sectionElement.getBoundingClientRect().top;
-    let linePosTop = Math.abs(titlePosTop - sectionPosTop);
-    lineElement.style.top = `${linePosTop + 22}px`;
-  });
-}
+    lineElements.forEach((lineElement) => {
+      let sectionElement = lineElement.closest(".section-flex");
+      let titleElement = sectionElement.querySelector(".title-bold");
+      const titlePosTop = titleElement.getBoundingClientRect().top;
+      const sectionPosTop = sectionElement.getBoundingClientRect().top;
+      let linePosTop = Math.abs(titlePosTop - sectionPosTop);
+      lineElement.style.top = `${linePosTop + 22}px`;
+    });
+  }
 
-alignLinesWithHeaders();
-
-window.onresize = () => {
   alignLinesWithHeaders();
+
+  window.onresize = () => {
+    alignLinesWithHeaders();
+  };
 };
