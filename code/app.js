f3a9871c2081b276ef658c0c0be92de092dc480e
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2023-11-09 07:26:33 +0200

    ds

--- code/app.js
+++ code/app.js
@@ -1,23 +1,21 @@
-window.onload = () => {
-  function alignLinesWithHeaders() {
-    let lineElements = document.querySelectorAll(".line");
+function alignLinesWithHeaders() {
+  let lineElements = document.querySelectorAll(".line");
 
-    lineElements.forEach((lineElement) => {
-      let sectionElement = lineElement.closest(".section-flex");
-      let titleElement = sectionElement.querySelector(".title-bold");
-      const titlePosTop = titleElement.getBoundingClientRect().top;
-      const sectionPosTop = sectionElement.getBoundingClientRect().top;
-      let linePosTop = Math.abs(titlePosTop - sectionPosTop);
-      var computedHeadStyle = window.getComputedStyle(titleElement);
-      var headerFontSize = computedHeadStyle.getPropertyValue("font-size");
-      headerFontSize = parseInt(headerFontSize);
-      lineElement.style.top = `${linePosTop + headerFontSize / 2}px`;
-    });
-  }
+  lineElements.forEach((lineElement) => {
+    let sectionElement = lineElement.closest(".section-flex");
+    let titleElement = sectionElement.querySelector(".title-bold");
+    const titlePosTop = titleElement.getBoundingClientRect().top;
+    const sectionPosTop = sectionElement.getBoundingClientRect().top;
+    let linePosTop = Math.abs(titlePosTop - sectionPosTop);
+    var computedHeadStyle = window.getComputedStyle(titleElement);
+    var headerFontSize = computedHeadStyle.getPropertyValue("font-size");
+    headerFontSize = parseInt(headerFontSize);
+    lineElement.style.top = `${linePosTop + headerFontSize / 2}px`;
+  });
+}
 
+alignLinesWithHeaders();
+
+window.onresize = () => {
   alignLinesWithHeaders();
-
-  window.onresize = () => {
-    alignLinesWithHeaders();
-  };
 };
